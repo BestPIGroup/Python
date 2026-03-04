7755d322cd8ccf9285936c5f557d4949039c274a
MatheusSouzaR <matheus.srodrigues@sptech.school>
2026-03-04 11:30:53 -0300

    Versão final do Monitoramento/Script

--- escrita.py
+++ escrita.py
@@ -6,34 +6,35 @@ from datetime import datetime
 def conversao_gb(valor: float):
     return valor/ (1024 ** 3)
 
-nome = "isa"
+nome = "Matheus"
 
 arquivo_csv = "monitoramento.csv"
 
-with open(arquivo_csv, mode="w",encoding="utf-8") as file:
-    writer = csv.writer(file, delimiter=",")
-    writer.writerow(["Nome", " Data", " CPU(%)", " Mémoria-total", " Mémoria-livre", " Mémoria-used", " Mémoria-free", " Mémoria(%)", " Disco-total", " Disco-used", " Disco-free"])
+with open(arquivo_csv, mode="w",  newline='', encoding="utf-8") as file:
+    writer = csv.writer(file)
+    writer.writerow(["Nome", " Data", " CPU(%)", " Mémoria-total", " Mémoria-livre", " Mémoria-used", " Mémoria-free", " Mémoria(%)", " Disco-total", " Disco-used", " Disco-free", " Disco(%)"])
 
+for i in range (1, 41):
+    cpu = psutil.cpu_percent(interval=1)
+    memoria = psutil.virtual_memory()
+    disco = psutil.disk_usage('/')
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-with open(arquivo_csv, mode="a",encoding="utf-8") as file:
-    writer = csv.writer(file, delimiter=";")
+    memoria_total_gb = conversao_gb(memoria.total)
+    memoria_available_gb = conversao_gb(memoria.available)
+    memoria_used_gb = conversao_gb(memoria.used)
+    memoria_free_gb = conversao_gb(memoria.free)
+    memoria_percent = memoria.percent
+ 
+    disco_total_gb = conversao_gb(disco.total)
+    disco_used_gb = conversao_gb(disco.used)
+    disco_free_gb = conversao_gb(disco.free)
+    disco_percent = disco.percent
 
-    for i in range (1, 41):
-        cpu = psutil.cpu_percent(interval=1)
-        memoria = psutil.virtual_memory()
-        disco = psutil.disk_usage('/')
-        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open(arquivo_csv, mode="a",  newline='', encoding="utf-8") as file:
+        writer = csv.writer(file, delimiter=";")
+        writer.writerow([nome, timestamp, cpu, round(memoria_total_gb, 2), round(memoria_available_gb, 2), round(memoria_used_gb, 2), round(memoria_free_gb, 2), memoria_percent, round(disco_total_gb, 2), round(disco_used_gb, 2), round(disco_free_gb, 2), disco_percent])
 
-        memoria_total_gb = conversao_gb(memoria.total)
-        memoria_available_gb = conversao_gb(memoria.available)
-        memoria_used_gb = conversao_gb(memoria.used)
-        memoria_free_gb = conversao_gb(memoria.free)
-        memoria.percent
+    print(f"Nome: {nome} | Data: {timestamp} - CPU: {cpu}% | Mem: {memoria_percent}% | Disk: {disco_percent}%")
 
-        disco_total_gb = conversao_gb(disco.total)
-        disco_used_gb = conversao_gb(disco.used)
-        disco_free_gb = conversao_gb(disco.free)
-
-        writer.writerow([nome, timestamp, cpu, round(memoria_total_gb, 2), round(memoria_available_gb, 2), round(memoria_used_gb, 2), round(memoria_free_gb, 2), round(disco_total_gb, 2), round(disco_used_gb, 2), round(disco_free_gb, 2)])
-
-        time.sleep(5)
+    time.sleep(5)
