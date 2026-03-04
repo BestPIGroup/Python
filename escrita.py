4e287ab19649ffa7fab92d6549d1a7f3e699daab
IsabelaTeixeiraRodrigues <isabela.rodrigues@sptech.school>
2026-03-04 11:13:54 -0300

    Versao atividade calculo

--- escrita.py
+++ escrita.py
@@ -6,12 +6,19 @@ from datetime import datetime
 def conversao_gb(valor: float):
     return valor/ (1024 ** 3)
 
+nome = "isa"
+
 arquivo_csv = "monitoramento.csv"
+
+with open(arquivo_csv, mode="w",encoding="utf-8") as file:
+    writer = csv.writer(file, delimiter=",")
+    writer.writerow(["Nome", " Data", " CPU(%)", " Mémoria-total", " Mémoria-livre", " Mémoria-used", " Mémoria-free", " Mémoria(%)", " Disco-total", " Disco-used", " Disco-free"])
+
 
 with open(arquivo_csv, mode="a",encoding="utf-8") as file:
     writer = csv.writer(file, delimiter=";")
 
-    while True:
+    for i in range (1, 41):
         cpu = psutil.cpu_percent(interval=1)
         memoria = psutil.virtual_memory()
         disco = psutil.disk_usage('/')
@@ -21,11 +28,12 @@ with open(arquivo_csv, mode="a",encoding="utf-8") as file:
         memoria_available_gb = conversao_gb(memoria.available)
         memoria_used_gb = conversao_gb(memoria.used)
         memoria_free_gb = conversao_gb(memoria.free)
+        memoria.percent
 
         disco_total_gb = conversao_gb(disco.total)
         disco_used_gb = conversao_gb(disco.used)
         disco_free_gb = conversao_gb(disco.free)
 
-        writer.writerow([timestamp, cpu, round(memoria_total_gb, 2), round(memoria_available_gb, 2), round(memoria_used_gb, 2), round(memoria_free_gb, 2), round(disco_total_gb, 2), round(disco_used_gb, 2), round(disco_free_gb, 2)])
+        writer.writerow([nome, timestamp, cpu, round(memoria_total_gb, 2), round(memoria_available_gb, 2), round(memoria_used_gb, 2), round(memoria_free_gb, 2), round(disco_total_gb, 2), round(disco_used_gb, 2), round(disco_free_gb, 2)])
 
-        time.sleep(2)
+        time.sleep(5)
