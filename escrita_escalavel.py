8647b673270048d05783b88bbfa28017c7cf4f7a
vmath-faria <victor.faria@sptech.school>
2026-04-09 15:30:53 -0300

    Reorganizei as métricas do banco JSON

--- escrita_escalavel.py
+++ escrita_escalavel.py
@@ -13,231 +13,178 @@ def conversao_gb(valor: float):
 
 def coletar_cpu_percent(parametros):
     return round(psutil.cpu_percent(interval=1),2)
-
 def coletar_cpu_count_logical(parametros):
     return round(psutil.cpu_count(),2)
-
 def coletar_cpu_count_physical(parametros):
     return round(psutil.cpu_count(logical=False),2)
-
 def coletar_cpu_freq_current(parametros):
     cpu = psutil.cpu_freq()
     return round(cpu.current,2)
-
 def coletar_cpu_freq_min(parametros):
     cpu = psutil.cpu_freq()
     return round(cpu.min,2)
-
 def coletar_cpu_freq_max(parametros):
     cpu = psutil.cpu_freq()
     return round(cpu.max,2)
-
 def coletar_cpu_ctx_switches(parametros):
     stats = psutil.cpu_stats()
     return round(stats.ctx_switches,2)
-
 def coletar_cpu_interrupts(parametros):
     stats = psutil.cpu_stats()
     return round(stats.interrupts,2)
-
 def coletar_cpu_soft_interrupts(parametros):
     stats = psutil.cpu_stats()
     return round(stats.soft_interrupts,2)
-
 def coletar_cpu_syscalls(parametros):
     stats = psutil.cpu_stats()
     return round(stats.syscalls,2)
-
 def coletar_cpu_time_user(parametros):
     tempos = psutil.cpu_times()
     return round(tempos.user,2)
-
 def coletar_cpu_time_system(parametros):
     tempos = psutil.cpu_times()
     return round(tempos.system,2)
-
 def coletar_cpu_time_idle(parametros):
     tempos = psutil.cpu_times()
     return round(tempos.idle,2)
-
+def coletar_virtual_memory_total(parametros):
+    memoria = psutil.virtual_memory()
+    return memoria.total
 def coletar_virtual_memory_total_gb(parametros):
     memoria = psutil.virtual_memory()
     return round(conversao_gb(memoria.total),2)
-
+def coletar_virtual_memory_available(parametros):
+    memoria = psutil.virtual_memory()
+    return memoria.available
 def coletar_virtual_memory_available_gb(parametros):
     memoria = psutil.virtual_memory()
     return round(conversao_gb(memoria.available),2)
-
 def coletar_virtual_memory_used_gb(parametros):
     memoria = psutil.virtual_memory()
     return round(conversao_gb(memoria.used),2)
-
 def coletar_virtual_memory_free_gb(parametros):
     memoria = psutil.virtual_memory()
     return round(conversao_gb(memoria.free),2)
-
 def coletar_virtual_memory_percent(parametros):
     memoria = psutil.virtual_memory()
     return round(memoria.percent,2)
-
 def coletar_virtual_memory_active_gb(parametros):
     memoria = psutil.virtual_memory()
     return round(conversao_gb(memoria.active),2)
-
 def coletar_virtual_memory_inactive_gb(parametros):
     memoria = psutil.virtual_memory()
     return round(conversao_gb(memoria.inactive),2)
-
 def coletar_virtual_memory_buffers_gb(parametros):
     memoria = psutil.virtual_memory()
     return round(conversao_gb(memoria.buffers),2)
-
 def coletar_virtual_memory_cached_gb(parametros):
     memoria = psutil.virtual_memory()
     return round(conversao_gb(memoria.cached),2)
-
 def coletar_swap_total_gb(parametros):
     swap = psutil.swap_memory()
     return round(conversao_gb(swap.total),2)
-
 def coletar_swap_used_gb(parametros):
     swap = psutil.swap_memory()
     return round(conversao_gb(swap.used),2)
-
 def coletar_swap_free_gb(parametros):
     swap = psutil.swap_memory()
     return round(conversao_gb(swap.free),2)
-
 def coletar_swap_percent(parametros):
     swap = psutil.swap_memory()
     return round(swap.percent,2)
-
 def coletar_swap_sin_gb(parametros):
     swap = psutil.swap_memory()
     return round(conversao_gb(swap.sin),2)
-
 def coletar_swap_sout_gb(parametros):
     swap = psutil.swap_memory()
     return round(conversao_gb(swap.sout),2)
-
 def coletar_disk_percent(parametros):
     disco = psutil.disk_usage('/')
     return round(disco.percent,2)
-
 def coletar_disk_total_gb(parametros):
     disco = psutil.disk_usage('/')
     return round(conversao_gb(disco.total),2)
-
 def coletar_disk_used_gb(parametros):
     disco = psutil.disk_usage('/')
     return round(conversao_gb(disco.used),2)
-
 def coletar_disk_free_gb(parametros):
     disco = psutil.disk_usage('/')
     return round(conversao_gb(disco.free),2)
-
 def coletar_disk_read_count(parametros):
     disco = psutil.disk_io_counters()
-    return round(disco.read_count,2)
-
+    return disco.read_count
 def coletar_disk_write_count(parametros):
     disco = psutil.disk_io_counters()
-    return round(disco.write_count,2)
-
+    return disco.write_count
 def coletar_disk_read_bytes_gb(parametros):
     disco = psutil.disk_io_counters()
     return round(conversao_gb(disco.read_bytes),2)
-
 def coletar_disk_write_bytes_gb(parametros):
     disco = psutil.disk_io_counters()
     return round(conversao_gb(disco.write_bytes),2)
-
 def coletar_disk_write_bytes(parametros):
     disco = psutil.disk_io_counters()
     return disco.write_bytes
-
 def coletar_disk_read_time(parametros):
     disco = psutil.disk_io_counters()
-    return round(disco.read_time,2)
-
+    return disco.read_time
 def coletar_disk_write_time(parametros):
     disco = psutil.disk_io_counters()
-    return round(disco.write_time,2)
-
+    return disco.write_time
 def coletar_net_bytes_sent_gb(parametros):
     rede = psutil.net_io_counters()
     return round(conversao_gb(rede.bytes_sent),2)
-
 def coletar_net_bytes_sent(parametros):
     rede = psutil.net_io_counters()
     return rede.bytes_sent
-
 def coletar_net_bytes_recv_gb(parametros):
     rede = psutil.net_io_counters()
     return round(conversao_gb(rede.bytes_recv),2)
-
 def coletar_net_bytes_recv(parametros):
     rede = psutil.net_io_counters()
     return rede.bytes_recv
-
 def coletar_net_packets_sent(parametros):
     rede = psutil.net_io_counters()
     return round(rede.packets_sent,2)
-
 def coletar_net_packets_recv(parametros):
     rede = psutil.net_io_counters()
     return round(rede.packets_recv,2)
-
 def coletar_net_errin(parametros):
     rede = psutil.net_io_counters()
     return round(rede.errin,2)
-
 def coletar_net_errout(parametros):
     rede = psutil.net_io_counters()
     return round(rede.errout,2)
-
 def coletar_net_dropin(parametros):
     rede = psutil.net_io_counters()
     return round(rede.dropin,2)
-
 def coletar_net_dropout(parametros):
     rede = psutil.net_io_counters()
     return round(rede.dropout,2)
-
 def coletar_total_processos(parametros):
     return round(len(psutil.pids()),2)
-
 def coletar_usuarios_logados(parametros):
     return round(len(psutil.users()),2)
-
 def coletar_boot_time(parametros):
     return round(psutil.boot_time(),2)
-
 def coletar_uptime_segundos(parametros):
     return round(time.time() - psutil.boot_time(),2)
-
 def coletar_uptime_minutos(parametros):
     return round((time.time() - psutil.boot_time())/60,2)
-
 def coletar_uptime_horas(parametros):
     return round((time.time() - psutil.boot_time())/3600,2)
-
 def coletar_total_particoes(parametros):
     particoes = psutil.disk_partitions()
     return round(len(particoes),2)
-
 def coletar_total_conexoes(parametros):
     conexoes = psutil.net_connections()
     return round(len(conexoes),2)
-
 def coletar_bateria_percent(parametros):
     bateria = psutil.sensors_battery()
     return round(bateria.percent,2)
-
 def coletar_bateria_plugada(parametros):
     bateria = psutil.sensors_battery()
     return round(bateria.power_plugged,2)
-
 def coletar_bateria_segundos_restantes(parametros):
     bateria = psutil.sensors_battery()
     return round(bateria.secsleft,2)
@@ -256,7 +203,9 @@ coletores = {
 "cpu_time_user": coletar_cpu_time_user,
 "cpu_time_system": coletar_cpu_time_system,
 "cpu_time_idle": coletar_cpu_time_idle,
+"virtual_memory_total": coletar_virtual_memory_total,
 "virtual_memory_total_gb": coletar_virtual_memory_total_gb,
+"virtual_memory_available": coletar_virtual_memory_available,
 "virtual_memory_available_gb": coletar_virtual_memory_available_gb,
 "virtual_memory_used_gb": coletar_virtual_memory_used_gb,
 "virtual_memory_free_gb": coletar_virtual_memory_free_gb,
@@ -320,7 +269,9 @@ resultados = {
 "cpu_time_user":"",
 "cpu_time_system":"",
 "cpu_time_idle":"",
+"virtual_memory_total":"",
 "virtual_memory_total_gb":"",
+"virtual_memory_available":"",
 "virtual_memory_available_gb":"",
 "virtual_memory_used_gb":"",
 "virtual_memory_free_gb":"",
